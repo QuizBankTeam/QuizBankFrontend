a6ed6e6426adad941773efa8b2d94ab5dc8ba50c
jacky <jacky8883938@gmail.com>
2023-10-12 15:26:29 +0800

    fdshfg

--- QuizBankTest/app/src/main/java/com/example/quizbanktest/adapters/quiz/QuestionAdapter.kt
+++ QuizBankTest/app/src/main/java/com/example/quizbanktest/adapters/quiz/QuestionAdapter.kt
@@ -50,7 +50,7 @@ class QuestionAdapter(private val context: Activity, private val questionList: A
         holder.questionDescription.text = currentItem.description
         Log.d("position is", "$position")
 
-        holder.questionImage.setImageResource(0)
+        holder.questionImage.setImageResource(R.drawable.notepad_and_pen_svgrepo_com)
         if(position<SingleQuiz.Companion.quizQuestionImages.size) {
             for (item in SingleQuiz.Companion.quizQuestionImages[position]) {
                 Log.d("position is $position", "contains img")
